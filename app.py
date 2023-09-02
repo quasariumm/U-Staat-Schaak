029b8cafd1424c9587f0d1f9f7ac48ab6a24bd85
quasariumm <patrickvreeburg06@gmail.com>
2023-09-02 16:05:09 +0200

    New kv format

--- app.py
+++ app.py
@@ -1,22 +1,49 @@
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.gridlayout import MDGridLayout
+from kivy.uix.button import Button
+from kivy.utils import get_color_from_hex
+import os, math, time
 
-KV="""
-<MainScreen>:
-    MDBoxLayout:
-        Button:
-            text:"Hello"
+board_prim = "#795C34"
+board_sec = "#E4D9CA"
 
-MainScreen:
-"""
+board = []
 
 class MainScreen(MDScreen):
     pass
 
+class ChessBoard(MDGridLayout):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rows = 8
+        self.cols = 8
+        self.orientation = 'lr-bt'
+        for i in range(1,65):
+            row = math.floor(i/8) + 1
+            square = Button(
+                background_color = 
+                    get_color_from_hex(board_prim) 
+                    if (i%2==0 if row%2==0 else i%2==1) else 
+                    get_color_from_hex(board_sec),
+                background_down = '',
+                background_normal = '' # Attribute in which the piece image is inserted
+            )
+            board.append(square)
+            self.add_widget(square, index=i-1)
+        print(len(self.children))
+
 class ChessApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.boardState = []
+
     def build(self):
-        return Builder.load_string(KV)
+        self.theme_cls.theme_style = 'Dark'
+
+        return Builder.load_file(os.path.dirname(__file__) + '\\app.kv')
+        
 
 if __name__ == "__main__":
     app = ChessApp()
